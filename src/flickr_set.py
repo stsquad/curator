9f6f9f1cf90cbb8a694a42270cbcb0c944f1d220
Alex Bennee <alex@bennee.com>
2010-09-23 12:17:18 +0100

    Implement basic info fetching from flickr

--- src/flickr_set.py
+++ src/flickr_set.py
@@ -7,15 +7,17 @@ import re
 
 from photo_set import photo_set
 from photo import photo
+from cache import cache
 
 # For flickrapi
-import xml.etree.ElementTree
+from xml.etree.ElementTree import ElementTree
 import flickrapi
+
+verbose=True
 
 #Secret: e377d23130b29828
 flickr_api_app_id="aveh_wzV34EyLrQovnSYnRxAUZPisqj30BJGWhoLfqSDIQ75nW4VvspQm1cbGaoelwE-"
 flickr_api_keys="2e99f9f42e759029f540360a36647abd"
-#flickr_api_keys=0x2e99f9f42e759029f540360a36647abd
 flickr = flickrapi.FlickrAPI(flickr_api_keys)
 
 
@@ -23,6 +25,11 @@ class flickr_set(photo_set):
     """
     A photoset that lives on the Flickr photo sharing site
     """
+
+    # set from flickr queries
+    info=None
+    title=None
+    set_size=0
 
     def __new__(cls, uri):
         """
@@ -52,31 +59,58 @@ class flickr_set(photo_set):
 
         >>> x=flickr_set("http://www.flickr.com/photos/someone/sets/12345678/with/10111213")
         >>> x.flickr_setid
-        12345678
+        '12345678'
         >>> print x
         Flickr based Photoset: http://www.flickr.com/photos/someone/sets/12345678/with/10111213 (0 photos)
         """
         super(self.__class__, self).__init__(uri)
 
         m = re.search("(set-|sets\/)([0-9]{1,})", uri);
-        pset = int(m.group(2))
+        pset=m.group(2)
         if pset:
             self.flickr_setid=pset
-            self.fetched=False
-            try:
-                sets = flickr.photosets_getInfo(photoset_id=pset)
-                self.fetched=True
-            except IOError:
-                # most likey can't talk to net
-                pass
-            except:
-                print "Unhandled exception caught:", sys.exc_type
+            self.cache = cache("flickr_"+str(self.flickr_setid))
+            ps_info=self._getInfo()
+            if ps_info:
+                # extract info from Photo Set
+                ps=ps_info.find("photoset")
+                self.set_size=ps.attrib['photos']
+                self.title=ps.find("title").text
+                self.desc=ps.find("description").text
+        else:
+            print "flickr_set: unable to extract set ID from URI:"+uri
+
+    def _getInfo(self):
+        """
+        Get basic information about the Flickr Set
+        """
+        self.info=None
+        try:
+            info=flickr.photosets_getInfo(photoset_id=self.flickr_setid)
+            self.info=ElementTree(info)
+            self.cache.save_xml("getInfo", etree)
+        except IOError:
+            # most likey can't talk to net, try the cache
+            self.info = self.cache.load_xml("getInfo")
+        except:
+            print "flickr_set: Unhandled exception ", sys.exc_type
+            raise sys.exc_type
+
+        return self.info
+        
 
     def __str__(self):
         """
         Print a string representation of the set
         """
-        return "Flickr based Photoset: %s (%d photos)" % (self.uri, len(self.photos))
+        string="Flickr based Photoset"
+        if self.title:
+            string=string+" '"+self.title+"'"
+        string=string+": "+self.uri+" ("
+        if self.set_size:
+            string=string+self.set_size+"/"
+        string=string+str(len(self.photos))+" photos)"
+        return string
 
 # Unit Tests
 if __name__ == "__main__":
@@ -84,7 +118,7 @@ if __name__ == "__main__":
     if len(sys.argv)>1 and not sys.argv[1].startswith("-v"):
         for a in sys.argv[1:]:
             print "Creating flickr_set: "+a
-            pset = file_set(a)
+            pset = flickr_set(a)
             if pset:
                 print "  created:"+str(pset)
             else:
