64e7fc2e24dfa4335c6964fa90378b6a9cc956b7
Alex Bennee <alex@bennee.com>
2010-10-24 14:54:24 +0100

    Fix thumbnal fetching for flickr photos

--- src/flickr_photo.py
+++ src/flickr_photo.py
@@ -3,6 +3,10 @@
 # Represents an individual photo in a set. 
 
 import urllib2
+
+# For flickrapi
+from xml.etree.ElementTree import ElementTree
+
 from photo import photo
 
 class flickr_photo(photo):
@@ -16,22 +20,11 @@ class flickr_photo(photo):
         self.flickr = flickr
         self.flickr_entry = flickr_entry
         self.cache = cache
-
-        try:
-            print "getting id..."
-            print "entry is: %s" % flickr_entry.dump()
-            id = flickr_entry.find("photo/id")
-            print "id is : %d" % (id)
-        except:
-            print "defaulting: flickr_entry = %s " % (dir(flickr_entry))
-            id = int(0)
-
-        print "flickr_photo: id is %s" % (id)
+        id = int(flickr_entry.get("id", 0))
         super(self.__class__, self).__init__(id)
 
-        if self.flickr:
-            print "Getting sizes...."
-            self.sizes = self.flickr.photos_getSizes(photo_id=id)
+        # Set on-demmand
+        self.sizes = None
 
     def __str__(self):
         """
@@ -42,18 +35,39 @@ class flickr_photo(photo):
         'flickr_123'
         """
         return str("flickr_"+str(self.id))
+    
+    def _getSizes(self):
+        if self.sizes is None:
+            try:
+                self.sizes = self.flickr.photos_getSizes(photo_id=self.id)
+                sizes_tree = ElementTree(self.sizes)
+                self.cache.save_xml("getSizes_"+str(self.id), sizes_tree)
+            except IOError:
+                # most likey can't talk to net, try the cache
+                sizes_tree = self.cache.load_xml("getSizes")
+                self.sizes = sizes_tree.getroot()
+
+        return self.sizes.find("sizes")
+            
+
 
     def get_icon_file_name(self):
-        file_name = "flickr_icon_"+self.id
+        file_name = "flickr_icon_"+str(self.id)+".jpg"
         file_path = self.cache.return_file_path(file_name)
         if file_path:
             return file_path
         else:
-            print "Downloading a thumbnail to: %s" % file_path
-            thumb = self.sizes.get("Thumbnail")
-            resp = urllib2.urlopen(thumb.get("source"))
-            self.cache.write_to_file(file_path)
-            return file_path
+            print "gifn: Downloading a thumbnail to: %s" % (file_name)
+            sizes = self._getSizes()
+
+            for el in sizes:
+                label = el.get('label')
+                if label == "Thumbnail":
+                    source = el.get('source')
+                    resp = urllib2.urlopen(source)
+                    return self.cache.write_file(file_name, resp.read())
+                
+        return None
 
 
 # Unit Tests
