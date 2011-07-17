14faa59a3f10984802fa5974a01afa4beeb27dcd
Alex Bennee <alex@bennee.com>
2011-07-17 22:09:01 +0100

    gui: almost got the concept of moving an active preview image forward
    There is obviously some fixing todo to ensure the image is in-front
    of it's peers.

--- ui/clutter/gui.py
+++ ui/clutter/gui.py
@@ -5,6 +5,44 @@
 
 import clutter
 
+class image_previews(clutter.Box):
+    """
+    A group of image previews
+    """
+    current_preview = None
+    current_index = 0
+    total_previews = 0
+    
+    def __init__(self, sets):
+
+        layout = clutter.FlowLayout(clutter.FLOW_HORIZONTAL)
+        layout.set_homogeneous(False)
+        layout.set_column_spacing(20)
+        layout.set_row_spacing(20)
+
+        clutter.Box.__init__(self, layout)
+        self.set_color(clutter.Color(0,0,0,200))
+        self.set_name('previews')
+        self.set_size(-1, -1)
+
+        for s in sets:
+            icon = clutter.Texture()
+            icon.set_from_file(s.get_icon_file())
+            for p in s.photos:
+                tex = clutter.Texture()
+                tex.set_from_file(p.get_icon_file_name())
+                self.add(tex)
+                self.total_previews += 1
+
+    def move_right(self):
+        if self.current_preview:
+            self.current_preview.set_scale(1.0, 1.0)
+        if self.current_index < self.total_previews:
+            self.current_index += 1
+            self.current_preview = self.get_children()[self.current_index]
+            self.current_preview.set_scale(3.0, 3.0)
+
+
 class GUI:
     """
     Class representing the GUI
@@ -48,42 +86,31 @@ class GUI:
         self.album_pane.set_size(100, -1)
         self.main_pane.add(self.album_pane)
 
-        preview_layout = clutter.FlowLayout(clutter.FLOW_HORIZONTAL)
-        preview_layout.set_homogeneous(True)
-        preview_layout.set_column_spacing(20)
-        preview_layout.set_row_spacing(20)
-
-        self.preview_pane = clutter.Box(preview_layout)
-        self.preview_pane.set_color(clutter.Color(0,0,0,200))
-        self.preview_pane.set_name('previews')
-        self.preview_pane.set_size(-1, -1)
-        self.main_pane.add(self.preview_pane)
-        
-
         # Fill the stage with preview icons and display
-        self.populate_preview()
-        self.stage.show_all()
-        
-    def populate_preview(self):
-        sets = self.control.get_sets()
-        for s in sets:
-            for p in s.photos:
-                tex = clutter.Texture()
-                tex.set_from_file(p.get_icon_file_name())
-                self.preview_pane.add(tex)
+        if self.control:
+            sets = self.control.get_sets()
+            self.previews = image_previews(sets)
+            self.main_pane.add(self.previews)
 
+        self.stage.show_all()
 
     def parse_keys(self, actor, event):
         #do stuff when the user presses a key
         #it would be awesome if I could find some documentation regarding clutter.keysyms
         if event.keyval == clutter.keysyms.q:
             #if the user pressed "q" quit the test
-            clutter.main_quit()
+            self.shutdown_gui()
+        elif event.keyval == clutter.keysyms.Right:
+            self.previews.move_right()
+        elif event.keyval == clutter.keysyms.Left:
+            print "left"
+        else:
+            print "un-handled key: %d, %s" % (event.keyval, str(hex(event.keyval)))
 
     def on_stage_resize(self, actor, allocation, flags):
         width, height = allocation.size
         self.album_pane.set_width(100)
-        self.preview_pane.set_width(width-100)
+        self.previews.set_width(width-100)
             
 
     def run(self):
